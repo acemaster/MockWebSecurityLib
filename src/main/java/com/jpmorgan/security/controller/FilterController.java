a9d3788df59c1727323e9969e9ae33209b38447b
acemaster <vivekhtc25@gmail.com>
2018-09-15 12:43:33 +0530

    Completed XSS filter

--- src/main/java/com/jpmorgan/security/controller/FilterController.java
+++ src/main/java/com/jpmorgan/security/controller/FilterController.java
@@ -1,6 +1,12 @@
 package com.jpmorgan.security.controller;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jpmorgan.security.lib.XSSUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +15,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+
 @Controller
 @RequestMapping("/testing")
 public class FilterController {
 
+    Logger logger = LoggerFactory.getLogger(FilterController.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @PostMapping
     public ResponseEntity getTestData(@RequestBody MockRequestBody mockRequestBody) {
+        try {
+            String temp = XSSUtil.stripXSS(objectMapper.writeValueAsString(mockRequestBody));
+            mockRequestBody = objectMapper.readValue(temp,MockRequestBody.class);
+        } catch (JsonProcessingException e) {
+            logger.info(e.getMessage());
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+        }
         return ResponseEntity.ok(String.format("<h1>Testing worked</h1><p>id: %s </p><p>Value: %s</p>",mockRequestBody.getId(),mockRequestBody.getTextBox()));
     }
 }
--- src/main/java/com/jpmorgan/security/request/XSSRequestWrapper.java
+++ src/main/java/com/jpmorgan/security/request/XSSRequestWrapper.java
@@ -2,12 +2,19 @@ package com.jpmorgan.security.request;
 
 import org.owasp.encoder.Encode;
 import org.owasp.encoder.Encoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletRequestWrapper;
 import java.util.regex.Pattern;
 
+import static com.jpmorgan.security.lib.XSSUtil.stripXSS;
+
 public class XSSRequestWrapper extends HttpServletRequestWrapper {
+
+    Logger logger = LoggerFactory.getLogger(XSSRequestWrapper.class);
+
     public XSSRequestWrapper(HttpServletRequest request) {
         super(request);
     }
@@ -15,7 +22,7 @@ public class XSSRequestWrapper extends HttpServletRequestWrapper {
     @Override
     public String[] getParameterValues(String parameter) {
         String[] values = super.getParameterValues(parameter);
-
+        logger.info("Values length: {}",values);
         if (values == null) {
             return null;
         }
@@ -42,55 +49,6 @@ public class XSSRequestWrapper extends HttpServletRequestWrapper {
         return stripXSS(value);
     }
 
-    private String stripXSS(String value) {
-        if (value != null) {
-            // NOTE: It's highly recommended to use the ESAPI library and uncomment the following line to
-            // avoid encoded attacks.
-            //value = Encode.canonicalize(value);
 
-            // Avoid null characters
-            value = value.replaceAll("", "");
-
-            // Avoid anything between script tags
-            Pattern scriptPattern = Pattern.compile("<script>(.*?)</script>", Pattern.CASE_INSENSITIVE);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid anything in a src='...' type of expression
-            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\'(.*?)\\\'", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            scriptPattern = Pattern.compile("src[\r\n]*=[\r\n]*\\\"(.*?)\\\"", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Remove any lonesome </script> tag
-            scriptPattern = Pattern.compile("</script>", Pattern.CASE_INSENSITIVE);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Remove any lonesome <script ...> tag
-            scriptPattern = Pattern.compile("<script(.*?)>", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid eval(...) expressions
-            scriptPattern = Pattern.compile("eval\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid expression(...) expressions
-            scriptPattern = Pattern.compile("expression\\((.*?)\\)", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid javascript:... expressions
-            scriptPattern = Pattern.compile("javascript:", Pattern.CASE_INSENSITIVE);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid vbscript:... expressions
-            scriptPattern = Pattern.compile("vbscript:", Pattern.CASE_INSENSITIVE);
-            value = scriptPattern.matcher(value).replaceAll("");
-
-            // Avoid onload= expressions
-            scriptPattern = Pattern.compile("onload(.*?)=", Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-            value = scriptPattern.matcher(value).replaceAll("");
-        }
-        return value;
-    }
 
 }
